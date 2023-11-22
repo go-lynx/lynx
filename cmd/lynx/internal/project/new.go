832f43f4e7ea98c525d167fbe8510d97a5cdf7c0
tanzhuo <tanzhuo@dingdandao.com>
2023-11-22 18:24:35 +0800

    feat:Add new CLI tool,Based on the Kratos project

---
+++ cmd/lynx/internal/project/new.go
@@ -0,0 +1,68 @@
+package project
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/fatih/color"
+
+	"github.com/go-lynx/lynx/cmd/lynx/internal/base"
+)
+
+// Project is a project template.
+type Project struct {
+	Name string
+	Path string
+}
+
+// New a project from remote repo.
+func (p *Project) New(ctx context.Context, dir string, layout string, branch string) error {
+	to := filepath.Join(dir, p.Name)
+
+	if _, err := os.Stat(to); !os.IsNotExist(err) {
+		fmt.Printf("🚫 %s already exists\n", p.Name)
+		prompt := &survey.Confirm{
+			Message: "📂 Do you want to override the folder ?",
+			Help:    "Delete the existing folder and create the project.",
+		}
+		var override bool
+		e := survey.AskOne(prompt, &override)
+		if e != nil {
+			return e
+		}
+		if !override {
+			return err
+		}
+		err := os.RemoveAll(to)
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Printf("🌟 Creating Lynx service %s, layout repo is %s, please wait a moment.\n\n", p.Name, layout)
+	repo := base.NewRepo(layout, branch)
+	if err := repo.CopyTo(ctx, to, p.Name, []string{".git", ".github"}); err != nil {
+		return err
+	}
+	e := os.Rename(
+		filepath.Join(to, "cmd", "user"),
+		filepath.Join(to, "cmd", p.Name),
+	)
+	if e != nil {
+		return e
+	}
+	base.Tree(to, dir)
+
+	fmt.Printf("\n🎉 Project creation succeeded %s\n", color.GreenString(p.Name))
+	fmt.Print("💻 Use the following command to start the project 👇:\n\n")
+	fmt.Println(color.WhiteString("$ cd %s", p.Name))
+	fmt.Println(color.WhiteString("$ go generate ./..."))
+	fmt.Println(color.WhiteString("$ go build -o ./bin/ ./... "))
+	fmt.Println(color.WhiteString("$ ./bin/%s -conf ./configs\n", p.Name))
+	fmt.Println("			🤝 Thanks for using Lynx")
+	fmt.Println("	📚 Tutorial: https://go-lynx.cn/docs/start")
+	return nil
+}
