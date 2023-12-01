4b2082e24def0e48722b1362428adb0039247d4f
tan-zhuo <tanzhuo@aliyun.com>
2023-12-01 11:03:21 +0800

    feat:add startup banner

--- app/logger.go
+++ app/logger.go
@@ -1,10 +1,15 @@
 package app
 
 import (
+	"embed"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/tracing"
+	"io/fs"
 	"os"
 )
+
+//go:embed banner.txt
+var configFS embed.FS
 
 func (a *LynxApp) InitLogger() {
 	log.Infof("Lynx Log component loading")
@@ -19,6 +24,11 @@ func (a *LynxApp) InitLogger() {
 	)
 	a.dfLog = log.NewHelper(a.logger)
 	log.Info("Lynx Log component loaded successfully")
+	data, err := fs.ReadFile(configFS, "banner.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	a.GetHelper().Info(string(data))
 }
 
 func (a *LynxApp) GetHelper() *log.Helper {
