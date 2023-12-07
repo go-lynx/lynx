84b381dd6bcf065327c8c1b2fb1fa1ab6c34ffb6
tan-zhuo <tanzhuo@aliyun.com>
2023-12-07 11:57:09 +0800

    fix:Improve GrpcSubscribe

--- app/subscribe.go
+++ app/subscribe.go
@@ -5,10 +5,10 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"github.com/go-kratos/kratos/v2/config"
-	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/tracing"
 	"github.com/go-kratos/kratos/v2/registry"
+	"github.com/go-kratos/kratos/v2/selector"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-lynx/lynx/conf"
 	gGrpc "google.golang.org/grpc"
@@ -17,60 +17,10 @@ import (
 type GrpcSubscribe struct {
 	name string
 	dis  registry.Discovery
-	log  log.Logger
 	tls  bool
 }
 
 type Option func(o *GrpcSubscribe)
-
-// Subscribe subscribe GRPC service
-func (g *GrpcSubscribe) Subscribe() *gGrpc.ClientConn {
-	dfLog := log.NewHelper(g.log)
-	endpoint := "discovery:///" + g.name
-	con, err := grpc.Dial(
-		context.Background(),
-		grpc.WithEndpoint(endpoint),
-		grpc.WithDiscovery(g.dis),
-		grpc.WithMiddleware(
-			logging.Client(g.log),
-			tracing.Client(),
-		),
-		grpc.WithTLSConfig(g.tlsLoad()),
-	)
-	if err != nil {
-		dfLog.Error(err)
-		panic(err)
-	}
-	return con
-}
-
-func (g *GrpcSubscribe) tlsLoad() *tls.Config {
-	if !g.tls {
-		return nil
-	}
-
-	s, err := Lynx().ControlPlane().Config("tls-root.yaml", g.name)
-
-	c := config.New(
-		config.WithSource(s),
-	)
-
-	if err := c.Load(); err != nil {
-		panic(err)
-	}
-
-	var t conf.Tls
-	if err := c.Scan(&t); err != nil {
-		panic(err)
-	}
-
-	certPool := x509.NewCertPool()
-	if !certPool.AppendCertsFromPEM([]byte(t.RootCA)) {
-		panic(err)
-	}
-
-	return &tls.Config{ServerName: g.name, RootCAs: certPool}
-}
 
 func WithServiceName(name string) Option {
 	return func(o *GrpcSubscribe) {
@@ -81,12 +31,6 @@ func WithServiceName(name string) Option {
 func WithDiscovery(dis registry.Discovery) Option {
 	return func(o *GrpcSubscribe) {
 		o.dis = dis
-	}
-}
-
-func WithLogger(log log.Logger) Option {
-	return func(o *GrpcSubscribe) {
-		o.log = log
 	}
 }
 
@@ -104,4 +48,61 @@ func NewGrpcSubscribe(opts ...Option) *GrpcSubscribe {
 		o(gs)
 	}
 	return gs
+}
+
+// Subscribe subscribe GRPC service
+func (g *GrpcSubscribe) Subscribe() *gGrpc.ClientConn {
+	if g.name == "" {
+		return nil
+	}
+
+	conn, err := grpc.Dial(
+		context.Background(),
+		grpc.WithEndpoint("discovery:///"+g.name),
+		grpc.WithDiscovery(g.dis),
+		grpc.WithMiddleware(
+			logging.Client(Lynx().GetLogger()),
+			tracing.Client(),
+		),
+		grpc.WithTLSConfig(g.tlsLoad()),
+		grpc.WithNodeFilter(g.nodeFilter()),
+	)
+	if err != nil {
+		Lynx().GetHelper().Error(err)
+		panic(err)
+	}
+	return conn
+}
+
+func (g *GrpcSubscribe) tlsLoad() *tls.Config {
+	if !g.tls {
+		return nil
+	}
+
+	if Lynx().ControlPlane() == nil {
+		return nil
+	}
+	s, err := Lynx().ControlPlane().Config("tls-root.yaml", g.name)
+	c := config.New(
+		config.WithSource(s),
+	)
+	if err := c.Load(); err != nil {
+		panic(err)
+	}
+	var t conf.Tls
+	if err := c.Scan(&t); err != nil {
+		panic(err)
+	}
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM([]byte(t.RootCA)) {
+		panic(err)
+	}
+	return &tls.Config{ServerName: g.name, RootCAs: certPool}
+}
+
+func (g *GrpcSubscribe) nodeFilter() selector.NodeFilter {
+	if Lynx().ControlPlane() == nil {
+		return nil
+	}
+	return Lynx().ControlPlane().NewNodeRouter(g.name)
 }
