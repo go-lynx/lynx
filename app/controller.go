93560a31dda4ce800a77d853fc3cb59830a57732
claire <claire@mastertech.one>
2024-12-25 21:38:37 +0800

    fix(v0.1.4-beta):完善中文备注信息

--- app/controller.go
+++ app/controller.go
@@ -77,24 +77,35 @@ func (a *LynxApp) SetControlPlane(plane ControlPlane) {
 }
 
 func (a *LynxApp) ControlPlaneBootConfiguration() config.Config {
+	// 检查控制平面是否已初始化，如果没有，则创建一个新的配置对象
 	if Lynx().ControlPlane() == nil {
 		return config.New()
 	}
 
-	// By default, load the application name + .yaml format file
+	// 默认情况下，加载应用程序名称 + .yaml 格式的文件
 	yaml := Name() + ".yaml"
+
+	// 记录日志，指示正在从配置中心读取文件，包括文件名、组名和命名空间
 	Lynx().Helper().Infof("Reading from the configuration center,file:[%v] group:[%v] namespace:[%v]", yaml, Name(), Lynx().ControlPlane().Namespace())
+
+	// 尝试从控制平面获取配置源，文件名是 yaml，组名是应用程序名称
 	s, err := Lynx().ControlPlane().Config(yaml, Name())
+	// 如果获取配置源时发生错误，记录错误并抛出 panic
 	if err != nil {
 		Lynx().Helper().Errorf("Failed to read the configuration file:[%v] group:[%v] namespace:[%v]", yaml, Name(), Lynx().ControlPlane().Namespace())
 		panic(err)
 	}
 
+	// 创建一个新的配置对象，使用获取到的配置源
 	c := config.New(config.WithSource(s))
+	// 加载配置，如果加载过程中发生错误，抛出 panic
 	if err := c.Load(); err != nil {
 		panic(err)
 	}
+
+	// 将全局配置设置为加载的配置
 	a.setGlobalConfig(c)
+	// 返回加载的配置对象
 	return c
 }
 
--- boot/conf.go
+++ boot/conf.go
@@ -8,21 +8,30 @@ import (
 
 // loadLocalBootFile Boot configuration file for service startup loaded from local
 func (b *Boot) loadLocalBootFile() {
+	// 打印日志，指示 Lynx 正在读取本地启动配置文件或文件夹
 	log.Info("Lynx reading local bootstrap configuration file/folder:" + flagConf)
+
+	// 创建一个新的配置对象，使用文件源加载配置
 	c := config.New(
 		config.WithSource(
 			file.NewSource(flagConf),
 		),
 	)
+
+	// 加载配置，如果加载过程中发生错误，抛出 panic
 	if err := c.Load(); err != nil {
 		panic(err)
 	}
-	// local file close
+
+	// 延迟关闭本地文件，确保在函数结束时关闭文件
 	defer func(c config.Config) {
+		// 关闭配置对象，如果关闭过程中发生错误，抛出 panic
 		err := c.Close()
 		if err != nil {
 			panic(err)
 		}
 	}(c)
+
+	// 将加载的配置对象保存到 Boot 结构体的 conf 字段中
 	b.conf = c
 }
