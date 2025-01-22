42272b36f00280152ee54ac67a789af1f30bef19
tan-zhuo <tanzhuo@aliyun.com>
2025-01-22 15:58:33 +0800

    fix(v1.2.0):update GrpcPlugin

--- plugins/internal/grpc/plug.go
+++ plugins/internal/grpc/plug.go
@@ -7,12 +7,26 @@ import (
 	"github.com/go-lynx/lynx/plugins"
 )
 
+// init registers the gRPC server plugin with the global plugin factory.
+// This function is automatically called when the package is imported.
+// It creates a new instance of ServiceGrpc and registers it with the
+// plugin factory using the configured plugin name and configuration prefix.
 func init() {
-	factory.GlobalPluginFactory().RegisterPlugin(name, confPrefix, func() plugins.Plugin {
-		return Grpc()
+	factory.GlobalPluginFactory().RegisterPlugin(pluginName, confPrefix, func() plugins.Plugin {
+		return NewServiceGrpc()
 	})
 }
 
+// GetServer retrieves the gRPC server instance from the plugin manager.
+// This function provides access to the underlying gRPC server for other
+// parts of the application that need to register services or access
+// server functionality.
+//
+// Returns:
+//   - *grpc.Server: The configured gRPC server instance
+//
+// Note: This function will panic if the plugin is not properly initialized
+// or if the plugin manager cannot find the gRPC plugin.
 func GetServer() *grpc.Server {
-	return app.Lynx().GetPluginManager().GetPlugin(name).(*ServiceGrpc).grpc
+	return app.Lynx().GetPluginManager().GetPlugin(pluginName).(*ServiceGrpc).server
 }
--- plugins/internal/grpc/tls.go
+++ plugins/internal/grpc/tls.go
@@ -7,20 +7,36 @@ import (
 	"github.com/go-lynx/lynx/app"
 )
 
+// tlsLoad creates and configures TLS settings for the gRPC server.
+// It performs the following operations:
+//   - Loads the X.509 certificate and private key pair
+//   - Creates a certificate pool and adds the root CA certificate
+//   - Configures TLS settings including client authentication type
+//
+// The method will panic if:
+//   - The certificate and key pair cannot be loaded
+//   - The root CA certificate cannot be added to the certificate pool
+//
+// Returns:
+//   - grpc.ServerOption: A configured TLS option for the gRPC server
 func (g *ServiceGrpc) tlsLoad() grpc.ServerOption {
+	// Load certificate and private key
 	tlsCert, err := tls.X509KeyPair(app.Lynx().Cert().GetCrt(), app.Lynx().Cert().GetKey())
 	if err != nil {
 		panic(err)
 	}
+
+	// Create certificate pool and add root CA
 	certPool := x509.NewCertPool()
 	if !certPool.AppendCertsFromPEM(app.Lynx().Cert().GetRootCA()) {
 		panic(err)
 	}
 
+	// Configure and return TLS settings
 	return grpc.TLSConfig(&tls.Config{
 		Certificates: []tls.Certificate{tlsCert},
 		ClientCAs:    certPool,
-		ServerName:   app.Name(),
+		ServerName:   app.GetName(),
 		ClientAuth:   tls.ClientAuthType(g.conf.GetTlsAuthType()),
 	})
 }
